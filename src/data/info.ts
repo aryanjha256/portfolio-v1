1a3b72d33cd8cd019f8f5a7236081beb703923b7
Aryan Kumar <aryanajha256@gmail.com>
2023-01-22 20:07:43 +0530

    first complete
    first commit after initial buildup of all the components

---
+++ src/data/info.ts
@@ -0,0 +1,142 @@
+export const info = {
+  baseUrl: "https://aryan-portfolio.vercel.app",
+  name: "Aryan Kumar",
+  jobDescription: "Python and Javascript Developer | Competitive Programmer",
+  about: `I'm a software engineer based in Jharkhand, India.
+   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
+   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
+   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
+   I have successfully completed several web projects from scratch. 
+   Yet, I learn continuously and never stop exploring new technologies.
+   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
+   firebase and React Native with time.
+  `,
+
+  techStack: [
+    {
+      type: "Programming Languages",
+      description: "I have worked with the following programming languages:",
+      list: [
+        "C",
+        "C++",
+        "C#",
+        "Java",
+        "Python",
+        "JavaScript",
+        "TypeScript",
+        "Dart",
+        "Rust",
+        "SQL",
+      ],
+    },
+    {
+      type: "Frameworks and Libraries",
+      description: "I have worked with the following frameworks and libraries:",
+      list: [
+        "NumPy",
+        "Pandas",
+        "Matplotlib",
+        "Seaborn",
+        "Scikit-learn",
+        "TensorFlow",
+        "Keras",
+        "PyTorch",
+        "Django",
+        "React JS",
+        "Next JS",
+        "Node JS",
+        "Express JS",
+        "React Native",
+        "Flutter",
+        "Bootstrap",
+        "Tailwind CSS",
+        "Styled Components",
+      ],
+    },
+    {
+      type: "Databases",
+      description: "I have worked with the following databases:",
+      list: ["Mongo DB", "MySQL", "Firebase", "Supabase", "PostgreSQL"],
+    },
+    {
+      type: "Tools",
+      description: "I have worked with the following tools:",
+      list: [
+        "Git",
+        "GitHub",
+        "VS Code",
+        "Postman",
+        "JetBrains IDEs",
+        "Netlify",
+        "Vercel",
+        "Heroku",
+        "AWS",
+        "Digital Ocean",
+      ],
+    },
+  ],
+
+  experience: [
+    {
+      name: "Chegg India (Subject Matter Expert)",
+      location: "Remote",
+      startDate: "Oct 2022",
+      endDate: "Present",
+      description: [
+        "- Developing screens and UI components for the web application using React and Tailwind.",
+        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Lovely Professional University",
+      location: "Phawara, Jalandhar, Punjab",
+      startDate: "2020",
+      endDate: "Present",
+      description: ["Bachelor of Technology, Computer Science and Enginnering"],
+    },
+    {
+      name: "Guru Gobind Singh Public School",
+      location: "Bokaro, Jharkhand",
+      startDate: "2017",
+      endDate: "2019",
+      description: ["Intermediate, PCM, Computer Science"],
+    },
+    {
+      name: "Kendriya Vidyalaya Godda",
+      location: "Godda, Jharkhand - 814133",
+      startDate: "2012",
+      endDate: "2018",
+      description: ["Matriculation"],
+    },
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/aryanjha256",
+    twitter: "https://twitter.com/aryanjha256",
+    github: "https://github.com/aryanjha256",
+    email: "aryanjha256@gmail.com",
+    linkedin: "https://www.linkedin.com/in/thearyankumar/",
+  },
+
+  projects: [
+    {
+      title: "Car Price Predictor",
+      isFeatured: true,
+      thumbnail: "/assets/images/car-price-predictor.jpg",
+      githubUrl: "https://github.com/aryanjha256/car-price-prediction",
+      liveUrl: "https://car-price-prediction-aryan.vercel.app/",
+      techStack: ["Python", "Numpy", "Pandas", "Sckit-learn"],
+    },
+    {
+      title: "Portfolio Website",
+      isFeatured: true,
+      thumbnail: "/assets/images/portfolio-website.jpg",
+      githubUrl: "https://github.com/aryanjha256",
+      liveUrl: "https://aryanjha256.github.io/",
+      techStack: ["Astro", "TypeScript", "Vercel"],
+    },
+  ],
+};
